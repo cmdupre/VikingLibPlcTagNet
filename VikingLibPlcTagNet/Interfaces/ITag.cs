5cbbabbe37d27f62a8c1bb9c0d89208be8a6fef9
cmd <caseydupre@gmail.com>
2026-01-29 12:51:08 -0600

    Added project and tests.

---
+++ VikingLibPlcTagNet/Interfaces/ITag.cs
@@ -0,0 +1,17 @@
+﻿using VikingLibPlcTagNet.Settings;
+
+namespace VikingLibPlcTagNet.Interfaces
+{
+    public interface ITag : IDisposable
+    {
+        TagPath Path { get; }
+        string FQN { get; }
+        int Id { get; }
+        string? Value { get; }
+        string? PreviousValue { get; }
+        bool Changed { get; }
+        void Write(string value, ILoggable? logger = null);
+        void Read(ILoggable? logger = null);
+        void Toggle(ILoggable? logger = null);
+    }
+}
